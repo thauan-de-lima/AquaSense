2df317c3d807183864aa309678ecf78498c3a32e
Thauan De Lima <thauanlima822@gmail.com>
2026-06-12 15:42:56 -0300

    feat: add DS18B20 temperature sensor reading

--- firmware/aquasense-firmware/src/main.cpp
+++ firmware/aquasense-firmware/src/main.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-void setup(){
-  pinMode(2, OUTPUT);
+#define SENSOR_PIN 4
 
+OneWire oneWire(SENSOR_PIN);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+  Serial.println("AquaSense - Sensor de Temperatura iniciado");
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
+  sensors.requestTemperatures(); //dedeqdfewdqewdqedqd
+  float temperatura = sensors.getTempCByIndex(0);
+
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+  Serial.println(" °C");
+
+  delay(2000);
 }
