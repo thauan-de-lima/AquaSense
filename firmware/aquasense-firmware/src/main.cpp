6397d926117332860a684df9046e255ae7bedaf0
Thauan De Lima <thauanlima822@gmail.com>
2026-06-12 17:29:43 -0300

    feat: add WiFi connection with credentials security

--- firmware/aquasense-firmware/src/main.cpp
+++ firmware/aquasense-firmware/src/main.cpp
@@ -1,20 +1,38 @@
+#include "credentials.h"
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <WiFi.h>
 
 #define SENSOR_PIN 4
 
 OneWire oneWire(SENSOR_PIN);
 DallasTemperature sensors(&oneWire);
 
+void conectarWiFi() {
+  Serial.print("Conectando ao Wi-Fi");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Wi-Fi conectado!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+}
+
 void setup() {
   Serial.begin(115200);
   sensors.begin();
+  conectarWiFi();
   Serial.println("AquaSense - Sensor de Temperatura iniciado");
 }
 
 void loop() {
-  sensors.requestTemperatures(); //dedeqdfewdqewdqedqd
+  sensors.requestTemperatures();
   float temperatura = sensors.getTempCByIndex(0);
 
   Serial.print("Temperatura: ");
