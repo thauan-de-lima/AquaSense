1771bf0b522cc6900c8f7da37c044a6db6350a50
Thauan De Lima <thauanlima822@gmail.com>
2026-06-12 19:48:33 -0300

    feat: add MQTT publishing to HiveMQ Cloud broker

--- firmware/aquasense-firmware/src/main.cpp
+++ firmware/aquasense-firmware/src/main.cpp
@@ -1,13 +1,18 @@
-#include "credentials.h"
+
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <WiFi.h>
+#include <WificlientSecure.h>
+#include <PubSubClient.h>
+#include "credentials.h"
 
 #define SENSOR_PIN 4
 
 OneWire oneWire(SENSOR_PIN);
 DallasTemperature sensors(&oneWire);
+WiFiClientSecure espClient;
+PubSubClient mqtt(espClient);
 
 void conectarWiFi() {
   Serial.print("Conectando ao Wi-Fi");
@@ -24,20 +29,46 @@ void conectarWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+void conectarMQTT() {
+  espClient.setInsecure();
+  mqtt.setServer(MQTT_HOST, MQTT_PORT);
+
+  Serial.print("Conectando ao broker MQTT");
+  while (!mqtt.connected()){
+    if (mqtt.connect("ESP32_AquaSense", MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("");
+      Serial.println("MQTT conectado!");
+    } else {
+      Serial.print(".");
+      delay(1000);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   sensors.begin();
   conectarWiFi();
+  conectarMQTT();
   Serial.println("AquaSense - Sensor de Temperatura iniciado");
 }
 
 void loop() {
+  if (!mqtt.connected()) {
+    conectarMQTT();
+  }
+  mqtt.loop();
+
   sensors.requestTemperatures();
   float temperatura = sensors.getTempCByIndex(0);
 
   Serial.print("Temperatura: ");
   Serial.print(temperatura);
   Serial.println(" °C");
+
+  String payload = String(temperatura);
+  mqtt.publish("aquasense/temperatura", payload.c_str());
+  Serial.println("Dado enviado ao broker MQTT!");
 
   if (temperatura > 30.0){
     Serial.println(" ALERTA: Temperatura ALTA!");
